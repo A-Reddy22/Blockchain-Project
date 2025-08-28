ac7613fe42ae80fa0b072e44ef02f988240853f6
Akshar Reddy <anreddy@ucsd.edu>
2025-08-27 22:58:22 -0700

    Initial blockchain API commit

--- blockchain.py
+++ blockchain.py
@@ -9,9 +9,11 @@ class Blockchain(object):
         self.chain=[]
         self.current_transactions=[]
     
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         #creates a new block and add it to the chain
-        pass
+        """ param proof: is an int, the proof given by the Proof of Work Algorithm
+        :param previous_hash:hash of previous block
+        :return<dict> New Block"""
 
     def new_transaction(self,sender,reciepent,amount):
         #adds a new transaction to the list of transactions
@@ -34,9 +36,31 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         #hashesh a block
-        pass
+        """
+        Creates a SHA-256 hash of a block
+        :param block:<dict> Block
+        returns 256 bit string
+        """
+        block_string=json.dumps(block,sort_keys=True).encode()
+        return
 
     @property
     def last_both(self):
         #returns the last block in the chain
         pass
+
+    def proof_of_work(self,last_proof):
+        """ 
+        A Proof of Work algorithm (PoW) is how new Blocks are created or mined on the blockchain. 
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         -
+        """
+
+        proof=0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+    
+    @staticmethod
